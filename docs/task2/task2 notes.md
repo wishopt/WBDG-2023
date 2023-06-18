f7c216c272c3332fa7bf33af126d5cad3b8c4931
wishopt <44169919+wishopt@users.noreply.github.com>
2023-06-18 18:13:42 +0200

    Task 2.1: Retrieve desk data dynamically

---
+++ docs/task2/task2 notes.md
@@ -0,0 +1,8 @@
+# Task 2
+
+## Retrieve desk data dynamically
+
+- Initially added the script tag in the head of index.html
+    - Event listeners didn't work because they were executed before the DOM loaded
+- JavaScript loops
+    - Some troubles with the differences between for, of and in when looping through arrays and objects
--- scripts.js
+++ scripts.js
@@ -1,5 +1,58 @@
-function testFunction() {
-    console.log("Hello World");
+const baseURL = "https://matthiasbaldauf.com/wbdg23"
+
+const contentDiv = document.getElementById("content");
+const buttons = {
+    displayAllDesks: document.getElementById("btnDisplayAllDesks"),
+    newBooking: document.getElementById("btnNewBooking"),
+    allBookings: document.getElementById("btnMyBookings")
+};
+
+async function displayAllDesks() {
+    const response = await fetch(baseURL + "/desks");
+    const desksData = await response.json();
+
+    clearContent();
+    generateTable(desksData);
 }
 
-testFunction();
+function clearContent() {
+    while (contentDiv.firstChild) {
+        contentDiv.removeChild(contentDiv.firstChild);
+    }
+};
+
+function generateTable(data) {
+    let table = document.createElement("table");
+    table.appendChild(generateTableHead(data[0]));
+
+    for (const property of data) {
+        let row = document.createElement("tr");
+        for (const [key, value] of Object.entries(property)) {
+            let cell = document.createElement("td");
+            if (key == "available") {
+                cell.innerHTML = (value == 1 ? "available" : "unavailable");
+            } else {
+                cell.innerHTML = value;
+            };
+            row.appendChild(cell);
+        };
+        table.appendChild(row);
+    };
+
+    contentDiv.appendChild(table);
+};
+
+function generateTableHead() {
+    let tableHead = document.createElement("tr");
+    let properties = ["ID", "Name", "Status", "Address", "Price", "Latitude", "Longitude", "Comment"];
+
+    for (const property of properties) {
+        let th = document.createElement("th");
+        th.innerHTML = property;
+        tableHead.appendChild(th);
+    };
+
+    return tableHead;
+};
+
+buttons.displayAllDesks.addEventListener("click", displayAllDesks);
